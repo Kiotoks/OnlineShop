59909c94b408cff374e0e16c21349a72ff987246
Cris <70279105+profeCrisPOO@users.noreply.github.com>
2023-08-22 11:15:19 -0300

    Update ShopOnLine.java

--- ShopOnLine.java
+++ ShopOnLine.java
@@ -6,11 +6,15 @@ import java.time.format.DateTimeFormatter;
 public class Shoponline {
 
     public static void main(String[] args) {
+        // modificar usando método en esta clase para permitir al usuario crear diferentes productos
         Producto r = new Remera('L', "Remera recta blanca", 2300);
         Producto p = new Pantalon(44,"Oxford", "Pantalon tiro alto", 4000);
         Producto a = new Accesorio(46, "Oro", "collar + dije", 3000);
+       
         Cliente c = new Cliente(11231, "Javier", "sexo",13453, 23423);
+        
         LocalDate f = LocalDate.now();
+       
         Venta v = new Venta(c,f);
         v.agregarProducto(p);
         v.agregarProducto(r);
